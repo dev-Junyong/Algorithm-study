09f584df11ee20f4e136c13be19deb2552c4c86c
Junyong Park <dev.junyong@gmail.com>
2022-01-08 21:56:28 +0900

    add Leetcode Read N Characters Given Read4

---
+++ Leetcode/22.01/157. Read N Characters Given Read4.py
@@ -0,0 +1,31 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        end = 0
+        temp = 4
+        buf_four = [''] * 4
+        while end < n and temp >= 4:
+            temp = read4(buf_four)
+            start = end
+            end = min(n, end + temp)
+            buf[start:end+1] = buf_four
+        return end
